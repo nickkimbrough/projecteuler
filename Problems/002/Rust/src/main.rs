1c69530f6d23fd888e7a8493b5d9b65adac77351
Nick Kimbrough <26373139+nickkimbrough@users.noreply.github.com>
2023-12-15 23:25:58 -0700

    Part 26 and 2 in Rust

---
+++ Problems/002/Rust/src/main.rs
@@ -0,0 +1,54 @@
+#![feature(test)]
+
+use std::{thread, time};
+
+extern crate test;
+
+/*
+    Trying a new implementation after reading the forums.
+
+    We know that phi is the golden ratio, and that every third number is even.
+    Given that, we can raise phi to the third power to calculate only the even
+    numbers of the sequence.
+
+    With some rounding applied, this might be the shortest method to get there.
+    I also got to learn about testing in Rust! It still only takes 0ns though.
+*/
+fn main() {
+    let sum = calculate_answer();
+    println!("{sum}");
+}
+
+fn calculate_answer() -> usize {
+    pub const PHI: f64 = 1.618033988749894848204586834365638118_f64;
+    let multiplier = PHI.powi(3);
+    let mut sum = 2;
+    let mut i = 2;
+    loop {
+        let value = (i as f64 * multiplier).round() as u128;
+
+        if value >= 4000000 {
+            break;
+        }
+
+        i = value;
+        sum += value as usize;
+    }
+    return sum;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4613732, calculate_answer());
+    }
+
+    #[bench]
+    fn bench_calculate_answer(b: &mut Bencher) {
+        b.iter(|| calculate_answer());
+    }
+}
--- Problems/026/Rust/src/main.rs
+++ Problems/026/Rust/src/main.rs
@@ -1,6 +1,50 @@
 /*
     Initial Thoughts:
 
-    TBD
+    Recurring cycles are going to be tricky to detect. I'm thinking of turning
+    the decimal fraction part into a string and doing some sort of growing
+    window search to find matching repeating cycles. The windows will always
+    be next to each other, but it won't always contain the entire decimal
+    fraction part.
+
+    I'm thinking about how many decimal fraction part places I need to calculate
+    and what would constitute a recurring cycle. Now that I think about it more,
+    I think I need a reptition of three to confirm a cycle.
+
+    This is getting a bit confusing, I'm going to go do some research, there's
+    probably a math shortcut.
+
+    Math Shortcut:
+
+    https://en.wikipedia.org/wiki/Repeating_decimal#A_shortcut
+
+    Particularly this part:
+    It follows that any repeating decimal with period n, and k digits after the
+    decimal point that do not belong to the repeating part, can be written as a
+    (not necessarily reduced) fraction whose denominator is (10n − 1)10k.
+
+    Conversely the period of the repeating decimal of a fraction c/d will be
+    (at most) the smallest number n such that 10n − 1 is divisible by d.
 */
-fn main() {}
+
+fn main() {
+    let mut n: usize = 0;
+    let mut max_period: usize = 0;
+
+    let mut i: usize = 1;
+    while i <= 1000 {
+        if i % 5 != 0 {
+            let mut p = 1;
+            while p < i && (10_usize.pow(p as u32)) % i != 1 {
+                p += 1;
+            }
+            if p > max_period {
+                max_period = p;
+                n = i;
+            }
+        }
+        i += 2;
+    }
+
+    print!("blah");
+}
