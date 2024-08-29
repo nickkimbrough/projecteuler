397c61bbf8b256eb54dcafb72ee7dbf879c2d3df
Nick Kimbrough <26373139+nickkimbrough@users.noreply.github.com>
2024-08-29 08:18:28 -0600

    Initialize Rust For 27 And Add Thoughts

---
+++ Problems/0027/Rust/src/main.rs
@@ -0,0 +1,14 @@
+/*
+    Initial Thoughts:
+
+    I think I'll need to write an efficient algorithm for checking the formula's
+    values with a set of given inputs. Since there are 1000 variations of a and
+    1001 variations of b, I think that means there are 1,001,000 different
+    possible combinations here. n could grow to a very large number as well.
+
+    My first attempt is going to be a brute force, but there may be some
+    mathematical shortcuts that I could take here to eliminate some of the
+    combinations that will arise after I start analyzing the data.
+*/
+
+fn main() {}
