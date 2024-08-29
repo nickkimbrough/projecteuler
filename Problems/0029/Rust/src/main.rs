9c2ff9e02285e8958038fcfcc8c036e3a9ab03a5
Nick Kimbrough <26373139+nickkimbrough@users.noreply.github.com>
2024-08-29 14:50:59 -0600

    Solve 29

--- Problems/0029/Rust/src/main.rs
+++ Problems/0029/Rust/src/main.rs
@@ -1,5 +1,8 @@
 #![feature(test)]
 
+use num_bigint::BigUint;
+use std::collections::HashSet;
+
 extern crate test;
 
 /*
@@ -15,17 +18,29 @@ extern crate test;
     Complexity here is going to be the nested for loops, we'll have 98*98
     iterations. I'm going to start with u128 values but I have a feeling I'm
     going to rather quickly start having to use large number libraries.
+
+    Solution Thoughts:
+
+    Yep, ran into large numbers, but Rust has great support for that. Brute
+    forcing worked really well:
+
+    tests::bench_calculate_answer_method_1 ... bench:   4,059,015.00 ns/iter (+/- 1,228,926.00)
+
+    With these speeds, I probably wouldn't bother much more.
 */
 fn main() {
     let d = calculate_answer_method_1();
     println!("Method 1 Answer: {d}");
 }
 
-fn calculate_answer_method_1() -> u128 {
-    for a in 2..101 {
-        for b in 2..101 {}
+fn calculate_answer_method_1() -> usize {
+    let mut terms: HashSet<BigUint> = HashSet::new();
+    for a in num_iter::range_inclusive(BigUint::from(2u8), BigUint::from(100u8)) {
+        for b in 2..101 {
+            terms.insert(a.pow(b));
+        }
     }
-    return 7;
+    return terms.iter().count();
 }
 
 #[cfg(test)]
@@ -35,7 +50,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(8675309, calculate_answer_method_1());
+        assert_eq!(9183, calculate_answer_method_1());
     }
 
     #[bench]
